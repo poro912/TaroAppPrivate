5f01ac113f5ccf74aa611429393e0a27de72ea14
poro <40479447+poro912@users.noreply.github.com>
2023-09-28 20:11:02 +0900

    upload GPT Class

--- kms/AITaroApp/app/src/main/java/TaroSystem/DataCenter.java
+++ kms/AITaroApp/app/src/main/java/TaroSystem/DataCenter.java
@@ -2,5 +2,33 @@ package TaroSystem;
 // 싱글톤으로 작동
 // 데이터를 모아놓는 공간
 public class DataCenter {
+	public static DataCenter instance;
+	private static JsonParser json_instance;
+	// 생성을 막아놓음
+	private DataCenter(){}
+
+	public DataCenter getInstance(){
+		if (instance == null) {
+			instance = new DataCenter();
+		}
+		return instance;
+	}
+
+	public void SaveResult(){
+
+	}
+
+	public void LoadResult(){
+
+	}
+
+	public void SaveAllResult(){
+
+	}
+
+	public void LoadAllResult(){
+
+	}
+
 
 }
--- kms/AITaroApp/app/src/main/java/TaroSystem/JsonParser.java
+++ kms/AITaroApp/app/src/main/java/TaroSystem/JsonParser.java
@@ -1,5 +1,25 @@
 package TaroSystem;
 
-public class JsonParser {
+//implementation 'com.fasterxml.jackson.core:jackson-databind:2.12.5' // 사용 가능한 최신 버전으로 업데이트
 
+public class JsonParser {
+	public static MyDataObject readJsonFile(Context context, String fileName) {
+		ObjectMapper objectMapper = new ObjectMapper();
+		AssetManager assetManager = context.getAssets();
+
+		try {
+			// Assets 폴더에서 JSON 파일을 읽어와 InputStream으로 열기
+			InputStream inputStream = assetManager.open(fileName);
+
+			// JSON 파일을 읽어와 Java 객체로 변환
+			MyDataObject dataObject = objectMapper.readValue(inputStream, MyDataObject.class);
+
+			// 읽어온 데이터 사용
+			return dataObject;
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return null;
+	}
 }
